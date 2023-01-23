3158d9bfe870221265e23ae028eb5fbb7ba7bc5e
Cédric Chevalier <cedric.chevalier@cea.fr>
2023-01-23 14:55:19 +0100

    Fix redefinition of constants

--- src/PhysicalConstants.cc
+++ src/PhysicalConstants.cc
@@ -12,11 +12,11 @@
    //    Energy       -  erg (g cm^2/s^2): in some background calculation
    //    Temperature  -  thousand electron-volts (keV)
 
-const double PhysicalConstants::_neutronRestMassEnergy = 9.395656981095e+2; /* MeV */
-const double PhysicalConstants::_pi = 3.1415926535897932;
-const double PhysicalConstants::_speedOfLight  = 2.99792458e+10;                // cm / s
+// const double PhysicalConstants::_neutronRestMassEnergy = 9.395656981095e+2; /* MeV */
+// const double PhysicalConstants::_pi = 3.1415926535897932;
+// const double PhysicalConstants::_speedOfLight  = 2.99792458e+10;                // cm / s
 
-// Constants used in math for computer science, roundoff, and other reasons
-const double PhysicalConstants::_tinyDouble           = 1.0e-13;
-const double PhysicalConstants::_smallDouble          = 1.0e-10;
-const double PhysicalConstants::_hugeDouble           = 1.0e+75;
+// // Constants used in math for computer science, roundoff, and other reasons
+// const double PhysicalConstants::_tinyDouble           = 1.0e-13;
+// const double PhysicalConstants::_smallDouble          = 1.0e-10;
+// const double PhysicalConstants::_hugeDouble           = 1.0e+75;
